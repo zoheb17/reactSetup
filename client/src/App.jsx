049aae52879943eda6aadeb71b9da66d1a2d048e
zoheb17 <sayyedzohebuddin61859@gmail.com>
2026-01-13 15:09:45 +0000

    react setup

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import react from 'react'
+
+import './App.css'
+
+const App=()=>{
+  return(
+    <>
+    <h1 className='text-center'> hello ibbu </h1>
+    </>
+  )
+}
+
+      
+       
+  
+
+export default App
